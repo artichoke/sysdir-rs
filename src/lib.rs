e85dd3290978411c80656b3ec220d162b9c91924
Ryan Lopopolo <rjl@hyperbo.la>
2023-05-10 22:38:40 -0700

    Add crate prelude, WIP crate docs, and wire in sys bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::cargo)]
+#![allow(unknown_lints)]
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
+#![warn(missing_docs)]
+#![warn(rust_2018_idioms)]
+#![warn(trivial_casts, trivial_numeric_casts)]
+#![warn(unused_qualifications)]
+#![warn(variant_size_differences)]
+// Enable feature callouts in generated documentation:
+// https://doc.rust-lang.org/beta/unstable-book/language-features/doc-cfg.html
+//
+// This approach is borrowed from tokio.
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_alias))]
+
+//! TODO
+
+#[allow(missing_docs)]
+#[allow(non_camel_case_types)]
+#[allow(clippy::all)]
+#[allow(clippy::pedantic)]
+#[allow(clippy::restriction)]
+pub mod sys;
 
 #[cfg(test)]
 mod tests {
