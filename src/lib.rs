53439a84dd0007e43f4e5348fb1b01618dcfab31
Ryan Lopopolo <rjl@hyperbo.la>
2023-05-10 23:19:20 -0700

    Add docs and tests
    This crate only exposes the raw bindings and will be a 'sys' crate.

--- src/lib.rs
+++ src/lib.rs
@@ -16,22 +16,116 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(docsrs, feature(doc_alias))]
 
-//! TODO
+//! Enumeration of the filesystem paths for the various standard system
+//! directories where apps, resources, etc. get installed.
+//!
+//! This crate exposes Rust bindings to the `sysdir(3)` library functions
+//! provided by `libSystem.dylib`.
+//!
+//! For more detailed documentation, refer to the [`sysdir(3)` man page](mod@man).
+//!
+//! # Platform Support
+//!
+//! The `sysdir` API first appeared in OS X 10.12, iOS 10, watchOS 3 and tvOS 10
+//! replacing the deprecated `NSSystemDirectories(3)` API.
+//!
+//! # Linkage
+//!
+//! `sysdir(3)` is provided by `libSystem`, which is linked into every binary on
+//! Apple platforms. This crate does not link to `CoreFoundation`, `Foundation`,
+//! or any other system libraries and frameworks.
+//!
+//! # Examples
+//!
+//! ```
+//! use core::ffi::{c_char, CStr};
+//!
+//! use sysdir::*;
+//!
+//! let mut path: [c_char; PATH_MAX as usize] = [0; PATH_MAX as usize];
+//!
+//! let dir = sysdir_search_path_directory_t::SYSDIR_DIRECTORY_USER;
+//! let domain_mask = sysdir_search_path_domain_mask_t::SYSDIR_DOMAIN_MASK_LOCAL;
+//!
+//! unsafe {
+//!     let mut state = sysdir_start_search_path_enumeration(dir, domain_mask);
+//!     loop {
+//!         let path = path.as_mut_ptr();
+//!         state = sysdir_get_next_search_path_enumeration(state, path);
+//!         if state == 0 {
+//!             break;
+//!         }
+//!         let path = CStr::from_ptr(path);
+//!         let s = path.to_str().unwrap();
+//!         assert_eq!(s, "/Users");
+//!     }
+//! }
+//! ```
 
+#![no_std]
+
+/// man page for `sysdir(3)`.
+///
+/// ```text
+#[doc = include_str!("../sysdir.3.man")]
+/// ```
+#[cfg(any(doc, doctest))]
+pub mod man {}
+
+/// Raw bindings to `sysdir(3)`, provided by `libSystem`.
+///
+/// The `sysdir` API first appeared in OS X 10.12, iOS 10, watchOS 3 and tvOS 10
+/// replacing the deprecated `NSSystemDirectories(3)` API.
 #[allow(missing_docs)]
 #[allow(non_camel_case_types)]
 #[allow(clippy::all)]
 #[allow(clippy::pedantic)]
 #[allow(clippy::restriction)]
-pub mod sys;
+mod sys;
+
+pub use self::sys::*;
 
 #[cfg(test)]
 mod tests {
+    use core::ffi::{c_char, CStr};
+
     use super::*;
 
+    // EXAMPLES
+    //
+    // ```c
+    // #include <limits.h>
+    // #include <sysdir.h>
+    //
+    // char path[PATH_MAX];
+    // sysdir_search_path_enumeration_state state = sysdir_start_search_path_enumeration(dir, domainMask);
+    // while ( (state = sysdir_get_next_search_path_enumeration(state, path)) != 0 ) {
+    //     // Handle directory path
+    // }
+    // ```
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn example_and_linkage() {
+        let mut count = 0_usize;
+        let mut path: [c_char; PATH_MAX as usize] = [0; PATH_MAX as usize];
+
+        let dir = sysdir_search_path_directory_t::SYSDIR_DIRECTORY_USER;
+        let domain_mask = sysdir_search_path_domain_mask_t::SYSDIR_DOMAIN_MASK_LOCAL;
+
+        unsafe {
+            let mut state = sysdir_start_search_path_enumeration(dir, domain_mask);
+            loop {
+                let path = path.as_mut_ptr();
+                state = sysdir_get_next_search_path_enumeration(state, path);
+                if state == 0 {
+                    break;
+                }
+                let path = CStr::from_ptr(path);
+                let s = path.to_str().unwrap();
+                assert_eq!(s, "/Users");
+                count += 1;
+            }
+        }
+
+        assert_eq!(count, 1, "Should iterate once and find `/Users`");
     }
 }
